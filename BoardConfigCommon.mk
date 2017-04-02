8524f1943d8a350d247b89df07974cae67c7a3a2
MOVZX <movzx@yahoo.com>
2017-04-02 04:50:28 +0300

    msm8956: Implement native DT2W support
    - Enable DT2W on the framework
    - Set PowerHAL DT2W target node
    - Add SEPolicy configurations
    
    Signed-off-by: MOVZX <movzx@yahoo.com>
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>
    Signed-off-by: Nick <nickgunale@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -200,6 +200,9 @@ TARGET_HAS_LEGACY_POWER_STATS := true
 TARGET_HAS_NO_WIFI_STATS := true
 TARGET_USES_INTERACTION_BOOST := true
 
+# DT2W
+TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
+
 # Properties
 TARGET_SYSTEM_PROP += $(VENDOR_PATH)/system.prop
 
