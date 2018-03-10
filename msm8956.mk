7fa241ebef09bbe824598c3192c0ae010725a14d
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-10 09:26:36 +0000

    msm8956-common: Use QTI HIDL power HAL
     * Let's use the new shiny binderized HAL in hardware/qcom/power
    
    Change-Id: I2abf4ca9b0b5a0cb031f12c400a1159962f5e94c
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -260,8 +260,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    power.msm8952
+    android.hardware.power@1.0-service-qti
 
 # QPerformance
 PRODUCT_BOOT_JARS += QPerformance
