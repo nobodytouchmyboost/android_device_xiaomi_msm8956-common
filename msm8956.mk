fecb3c571e66311761f2db8241daf67a9075a36b
Nick <nickgunale@gmail.com>
2018-07-06 13:03:08 +0530

    msm8956-common: Add perfboostsconfig and power-profile to tree
    Signed-off-by: Nick <nickgunale@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -312,6 +312,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-service-qti
 
+# Perf config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+    $(LOCAL_PATH)/configs/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+
 # Qualcomm dependencies
 PRODUCT_PACKAGES += \
     libtinyxml \
